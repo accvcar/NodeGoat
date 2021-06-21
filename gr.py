c63161e992525c88c2c03f6fa37e23dff07c7d79
VC <victor.c.lourenco@accenture.com>
2021-06-21 19:01:32 -0300

    add python

--- gr.py
+++ gr.py
@@ -2,15 +2,15 @@ import sys
 import requests
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-
 api_base = "https://api.veracode.com/greenlight/v3"
 headers = {
   'FILES-TO-SCAN': '/home/sql/NodeGoat/app/routes/contributions.js',
   'PROJECT-URI': 'https://github.com/accvcar/NodeGoat',
   "User-Agent": "Python HMAC Example"
 }
+
 #'PROJECT-NAME': 'NodeGoat-Azure',
-############
+
 if __name__ == "__main__":
 
     try:
