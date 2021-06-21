f94cb4e2f4b7f675f6628981cfee33053a9e0f29
root <root@CPX-RA6MQJ8HW08.dir.svc.accenture.com>
2021-06-21 18:39:34 -0300

    upload python

---
+++ gr.py
@@ -0,0 +1,36 @@
+import sys
+import requests
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+
+
+api_base = "https://api.veracode.com/greenlight/v3"
+headers = {
+  'FILES-TO-SCAN': '/home/sql/NodeGoat/app/routes/contributions.js',
+  'PROJECT-URI': 'https://github.com/accvcar/NodeGoat',
+  "User-Agent": "Python HMAC Example"
+}
+#'PROJECT-NAME': 'NodeGoat-Azure',
+if __name__ == "__main__":
+
+    try:
+        response = requests.get(api_base + "/scan/js", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+    except requests.RequestException as e:
+        print("Whoops!")
+        print(e)
+        sys.exit(1)
+
+    if response.ok:
+        data = response.json()
+#        for app in data["_embedded"]["applications"]:
+        for app in data:
+            print(app)
+    else:
+        #print(response.status_code)
+        print(response)
+
+
+
+
+
+        
+        zip -r upload.zip /home/sql/NodeGoat/app/routes/contributions.js
